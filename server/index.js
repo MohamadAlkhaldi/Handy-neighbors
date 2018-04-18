1189d6297090cf78058d3873dce26fca306a0cc8
Nooreddein <nonosyousef@gmail.com>
2018-04-18 23:37:51 +0300

    encryption and some changes in react

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var db = require('../database-mongo/index.js');
 var app = express();
+var bcrypt = require('bcrypt')
+var saltRounds = 10
 //----------------------------------------------------------
 app.use(express.static(__dirname + '/../react-client/dist'));
 //----------------------------------------------------------
@@ -20,19 +22,68 @@ app.use(bodyParser.json())
   
 // });
 
-app.post('/', function (req, res) {
+app.post('/signup', function (req, res) {
   var data=req.body;
-  db.findOne(data.username,function(err,datares){
-  	if(err){
-  		res.send(err)
-  	}
-  	res.redirect('index');
-  })
+bcrypt.hash(data.password,saltRounds,function(err,hash){
+	if(err){
+		console.log(err)
+	}db.save({
+			username:req.body.username,
+			password:hash
+		},function(err,data){
+			if(err){
+				console.log(err)
+			}
+			res.send(data)
+		})
+		
+})
+// var hash = bcrypt.hashSync(data.password, saltRounds);
+// db.save({username:data.username , password : hash},function(err,data){
+// 	if(err){
+// 		console.log(err)
+// 	}
+// 	console.log(data)
+// 	res.redirect("/login")
+// })
+
+
+
+});
+app.get('/signup', function (req, res) {
+  var data=req.body;
+
  	
-  
+  console.log('wseu')
 });
 
-app.listen(3000, function() {
+
+// app.post('/signin', function (req, res) {
+//   var data=req.body;
+//   db.findOne(data.username,function(err,datares){
+//   	if(err){
+//   		res.send(err)
+//   	}
+//   	res.redirect('index');
+//   })
+ 	
+  
+// });
+
+// app.get('/signin', function (req, res) {
+//   var data=req.body;
+//   db.findOne(data.username,function(err,datares){
+//   	if(err){
+//   		res.send(err)
+//   	}
+//   	res.redirect('index');
+//   })
+ 	
+  
+// });
+
+
+app.listen(3001, function() {
   console.log('listening on port 3000!');
 });
 
