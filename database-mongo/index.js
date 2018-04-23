96379bb9c06ee4d291e5addebdf8c8130f161396
Ammarlio <ammarhalbouni@gmail.com>
2018-04-23 18:33:46 +0300

    mlab

--- database-mongo/index.js
+++ database-mongo/index.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-mongoose.connect("mongodb://ammarlio:ammarlion123@ds115131.mlab.com:15131/ammar");
+mongoose.connect("mongodb://ammarlio:ammarlion123@ds255319.mlab.com:55319/handy-neighbors");
 
 var db = mongoose.connection;
 
