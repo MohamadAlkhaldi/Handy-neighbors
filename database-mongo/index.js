94f64e5afa9e985d71d336022161e49fc154c086
Ammarlio <ammarhalbouni@gmail.com>
2018-04-24 13:36:08 +0300

    edits

--- database-mongo/index.js
+++ database-mongo/index.js
@@ -11,57 +11,31 @@ db.once('open', function() {
   console.log('mongoose connected successfully');
 });
 
-// var userSchema = mongoose.Schema({
-//   username: String,
-//   password: String,
-//   phonenumber:Number,
-//   email:String
-// });
 
-var techSchema = mongoose.Schema({
-  username:String,
-  password:String,
-  phonenumber:Number,
-  longitude:String,
-  laltitude:String,
-  distance:Number,
-  services:Array
-})
-
-// var User = mongoose.model('User', userSchema);
-var Technitian = mongoose.model('Tecnitian',techSchema)
-// var selectAll = function(callback) {
-//   User.find({}, function(err, Users) {
-//     if(err) {
-//       callback(err, null);
-//     } else {
-//       callback(null, Users);
-//     }
-//   });
-// };
-// var noor = new User({username:'noor',password:'noor',phonenumber:12345678})
-//    noor.save(function(err){
-//     if(err){
-//     console.log(err)
-
-//     }
-//   })
-
-
-
-// var x = 0  
-var save = function(data,callback){
-  var user = new Technitian(data)
-  user.save(function(err,dataRes){
-    if(err){
-      callback(err,null)
-
-    }
-    callback(null,dataRes)
+  var techSchema = mongoose.Schema({
+    username:String,
+    password:String,
+    phonenumber:Number,
+    longitude:String,
+    laltitude:String,
+    distance:Number,
+    services:Array
   })
-}
 
-// module.exports.selectAll = selectAll;
+
+  var Technitian = mongoose.model('Tecnitian',techSchema)
+   
+  var save = function(data,callback){
+    var user = new Technitian(data)
+    user.save(function(err,dataRes){
+      if(err){
+        callback(err,null)
+
+      }
+      callback(null,dataRes)
+    })
+  }
+
 module.exports.Technitian = Technitian;
 module.exports.save = save;
 
