f00ec0b87ec4033f5079080f17cfba0dcb1753b3
mohamadalkhaldi <mmalkhaldi119@cit.just.edu.jo>
2018-04-28 13:15:07 +0300

    front end email

--- react-client/src/MechList.jsx
+++ react-client/src/MechList.jsx
@@ -12,7 +12,7 @@ class MechList extends React.Component{
             return(
                 <div style={{border:'solid', 'borderRadius': '15px', 'borderColor' : '#E9AB17'}}>
                 <ul style={{backgrondColor: '#E44F4F'}} className="list-group">
-                {this.props.mechs.map((mech)=><Mechanic mech={mech} key={'d'+mech.username}/>)}
+                {this.props.mechs.map((mech)=><Mechanic mech={mech} key={'d'+mech.username} longitude={this.props.longitude} laltitude={this.props.laltitude}/>)} 
                 </ul>
                 </div>
                 )
